2dff518d49e2912bb7e1a20a0a7666abc982208d
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2014-08-23 10:15:30 +0200

    Highlights the selected chapter

--- client/js/directives/chapter-headline.js
+++ client/js/directives/chapter-headline.js
@@ -1,18 +1,20 @@
 'use strict';
 
-angular.module('app.chapter-headline', []).directive('chapterHeadline', ['$location', 'Model', playerControlsDirective]);
+angular.module('app.chapter-headline', []).directive('chapterHeadline', ['$location', 'Model', chapterHeadlineDirective]);
 
-function playerControlsDirective($location, Model) {
+function chapterHeadlineDirective($location, Model) {
   return {
     restrict: 'E',
-    scope: {
-      'video': '='
-    },
     replace: false,
+    scope: true,
     link: function (scope, element, attrs) {
 
     },
     controller: function ($scope, $element) {
+      $scope.isSelected = function(chIndex) {
+        return Model.getChapterIndex() == chIndex;
+      };
+
       $scope.play = function (chIndex) {
         $location.path('/play/' + $scope.video.id + '/' + chIndex);
       };
