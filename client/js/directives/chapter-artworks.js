49e3c9740c8fc8b469a2db4916c2014e29bcf6ef
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2014-08-25 18:24:34 +0200

    Fetches chapter artworks from Europeana

--- client/js/directives/chapter-artworks.js
+++ client/js/directives/chapter-artworks.js
@@ -14,44 +14,31 @@ function chapterArtworksDirective(europeanaApi, Model) {
 
     },
     controller: function ($scope, $element) {
+      $scope.artworks = [];
       $scope.loading = false;
 
-      loadChapterInformation();
+      loadChapterArtworks();
 
-      // The chapter information are shown in the first information card
-      function loadChapterInformation() {
-        var metadata = _.chain($scope.chapter.fragments)
-          .map(function (f) {
-            return {value: f.title.trim(), uri: f.locator.trim()}
-          })
-          .filter(function (e) {
-            return e.value.length > 0
-          })
-          .uniq(false, function (e) {
-            return e.value;
-          })
-          .value();
+      function loadChapterArtworks() {
+        var metadataSize = $scope.metadata.length;
 
-        //metadata.push({value: 'Piet Mondrian', uri: 'http://dbpedia.org/resource/Piet_Mondrian'}); //for testing navigation uncomment this
-
-        var chapterTitle = $scope.chapter.title;
-
-        var chapterEntity = {value: chapterTitle, uri: ''};
-        callEuropeanaApi(chapterEntity);
-      }
-
-      function callEuropeanaApi(e) {
         $scope.loading = true;
-        europeanaApi.get({query: e.value}, function (r) {
-          console.log(r);
+        _($scope.metadata).each(function (m, index) {
+          europeanaApi.get({query: m.value}, function (r) {
+            if (r.itemsCount > 0) {
+              _(r.items).each(function (i) {
+                if (i.edmPreview && i.title) {
+                  $scope.artworks.push({img: i.edmPreview[0], title: i.title[0]});
+                }
+              });
+            }
 
-          $scope.loading = false;
+            if (index == metadataSize - 1) {
+              $scope.loading = false;
+            }
+          });
         });
       }
-
-      $scope.proxy = function (entity) {
-        callEuropeanaApi(entity)
-      };
     },
     templateUrl: 'partials/directives/chapter-artworks.html'
   }
