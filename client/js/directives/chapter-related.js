ea6cb4eca822028dd6601c7c54f4e6775ff93524
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2014-09-07 18:41:50 +0200

    Fixes related chapters

--- client/js/directives/chapter-related.js
+++ client/js/directives/chapter-related.js
@@ -8,7 +8,25 @@ function chapterRelatedDirective(Model) {
     scope: {},
     replace: false,
     link: function (scope, element, attrs) {
-      scope.relatedVideos = Model.getVideos();
+      scope.$watch(
+        function () {
+          return Model.getVideos();
+        },
+        function (newVideos) {
+          if (newVideos.length > 0) {
+            scope.relatedVideos = newVideos;
+          }
+        }
+      );
+
+      scope.getShot = function (video, chapter) {
+        var d = new Date(chapter.startTime);
+        var h = d.getHours() - 1;
+        var m = d.getMinutes();
+        var s = d.getSeconds();
+        return video.shots + "/h/" + h + "/m/" + m + "/sec" + s + ".jpg";
+      };
+
     },
     templateUrl: 'partials/directives/chapter-related.html'
   }
