3e746e4ff8379c88c89c0bbbadbc1171bd84a3a9
Michiel Hildebrand <m.hildebrand@vu.nl>
2014-09-08 22:13:18 +0200

    remove debuging config

--- client/js/directives/chapter-enrich.js
+++ client/js/directives/chapter-enrich.js
@@ -23,7 +23,7 @@ function chapterEnrichDirective() {
     controller: function ($scope) {
       $scope.crumb = [];
       $scope.content = null;
-      $scope.activeLinks = "chapters";
+      $scope.activeLinks = "about";
 
       $scope.showLinks = function (active) {
         $scope.activeLinks = active;
