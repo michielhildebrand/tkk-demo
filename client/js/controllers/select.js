578e5eb108bc27a28a9a73fc69ef7af0ef0d2115
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2014-09-15 17:18:35 +0200

    Fixes formatting

--- client/js/controllers/select.js
+++ client/js/controllers/select.js
@@ -1,15 +1,15 @@
 'use strict';
 
-angular.module('SelectCtrl', []).controller('SelectCtrl', ['$scope', '$state','Config', selectCtrl]);
+angular.module('SelectCtrl', []).controller('SelectCtrl', ['$scope', '$state', 'Config', selectCtrl]);
 
 function selectCtrl($scope, $state, Config) {
   $scope.users = Config.users;
 
-  $scope.goToEpisodes = function(user) {
+  $scope.goToEpisodes = function (user) {
     $state.go('episodes', {user: user});
   };
 
-  $scope.goToTV = function(user) {
+  $scope.goToTV = function (user) {
     $state.go('tv', {user: user});
   }
 }
