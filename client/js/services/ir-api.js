ec4879a1b1e7543963be0afa77203bc32fb0f529
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2014-11-05 10:42:24 +0100

    Adds thd parameter for filtering the IRApi response

--- client/js/services/ir-api.js
+++ client/js/services/ir-api.js
@@ -3,7 +3,7 @@
 angular.module('IRApi', ['ngResource']).factory('irApi', ['$resource', 'Config', irApiResource]);
 
 function irApiResource($resource, Config) {
-  return $resource(Config.IR_API + '?q=:query', {query: '@query'},
+  return $resource(Config.IR_API + '?q=:query&thd=yes', {query: '@query'},
     {
       search: {
         method: 'GET'
