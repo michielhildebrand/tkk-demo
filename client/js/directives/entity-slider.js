31582a512b101efcafcba214b5478e9817fe90f0
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2014-11-13 01:50:46 +0100

    Adds fragment watch Reindents code

--- client/js/directives/entity-slider.js
+++ client/js/directives/entity-slider.js
@@ -3,40 +3,60 @@
 angular.module('app.entity-slider', []).directive('entitySlider', ['$log', 'entityProxy', 'Model', entitySliderDirective]);
 
 function entitySliderDirective($log, entityProxy, Model) {
-    return {
-        restrict: 'E',
-        scope: {
+  return {
+    restrict: 'E',
+    scope: {},
+    replace: false,
+    link: function (scope, element, attrs) {
+
+      scope.$watch(
+        function () {
+          return Model.getChapter()
         },
-        replace: false,
-        link: function (scope, element, attrs) {
+        function (newChapter) {
+          if (newChapter != null && newChapter.fragments != null) {
+            setEntityContent(newChapter.fragments);
+          }
+        }
+      );
 
-            scope.$watch(
-                function () {
-                    return Model.getChapter()
-                },
-                function (newChapter) {
-                    if (newChapter != null && newChapter.fragments != null) {
-                        setEntityContent(newChapter.fragments);
-                    }
-                }
-            );
-
-            function setEntityContent(entities) {
-                var urls =  _.chain(entities)
-                    .filter(function(e) { return e.locator; })
-                    .sortBy(function(e) { return e.startTime; })
-                    .map(function(e) { return e.locator; })
-                    .value();
-
-                console.log(urls);
-
-                entityProxy.getList({urls:angular.toJson(urls)}, function (r) {
-                    console.log(r);
-                    scope.entities = r;
-                });
-            }
+      scope.$watch(
+        function () {
+          return Model.getFragment()
         },
+        function (newFragment) {
+          if (newFragment != null) {
+            entityProxy.getList({urls: angular.toJson(newFragment)}, function (res) {
+              debug('Set information card content: ' + JSON.stringify(res[newFragment]));
+              scope.currentEntity = res;
+            });
+          }
+        }
+      );
 
-        templateUrl: 'partials/directives/entity-slider.html'
-    }
+      function setEntityContent(entities) {
+        var urls = _.chain(entities)
+          .filter(function (e) {
+            return e.locator;
+          })
+          .sortBy(function (e) {
+            return e.startTime;
+          })
+          .map(function (e) {
+            return e.locator;
+          })
+          .value();
+
+        entityProxy.getList({urls: angular.toJson(urls)}, function (res) {
+          debug('Loading ' + _(res).keys().length + ' entities.');
+          scope.entities = res;
+        });
+      }
+
+      function debug(msg) {
+        $log.debug('[Entity Slider (directive)] ' + msg)
+      }
+    },
+    templateUrl: 'partials/directives/entity-slider.html'
+  }
 }
