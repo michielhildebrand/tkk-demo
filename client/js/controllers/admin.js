121bdb6412965a294b9867147e23781450d5d7d8
Michiel Hildebrand <m.hildebrand@cwi.nl>
2014-11-16 21:41:09 +0100

    Load from seed file

--- client/js/controllers/admin.js
+++ client/js/controllers/admin.js
@@ -1,23 +1,1 @@
-'use strict';
-
-angular.module('AdminCtrl', []).controller('AdminCtrl', ['$scope', 'Model', '$log', adminCtrl]);
-
-function adminCtrl($scope, Model, $log) {
-
-  $scope.$watch(
-      function () {
-        return Model.getVideos();
-      },
-      function (newVideos) {
-        console.log(newVideos);
-        if (newVideos.length > 0) {
-          $scope.videos = newVideos;
-        }
-      }
-  );
-
-  function debug(msg) {
-    $log.debug('[Admin (ctrl)] ' + msg)
-  }
-
-}
+'use strict';angular.module('AdminCtrl', []).controller('AdminCtrl', ['$scope', '$http', 'Config', '$log', adminCtrl]);function adminCtrl($scope, $http, Config, $log) {        $http.get(Config.seed).success(function(data) {        $scope.videos = data;    });  function debug(msg) {    $log.debug('[Admin (ctrl)] ' + msg)  }}
