d73fe7a193ace6c0f9dd92b04477e0bdd0eca3b6
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2014-11-16 21:42:21 +0100

    Adds limit param to irapi service

--- client/js/services/ir-api.js
+++ client/js/services/ir-api.js
@@ -3,7 +3,7 @@
 angular.module('IRApi', ['ngResource']).factory('irApi', ['$resource', 'Config', irApiResource]);
 
 function irApiResource($resource, Config) {
-  return $resource(Config.IR_API + '?q=:query&thd=yes', {query: '@query'},
+  return $resource(Config.IR_API + '?q=:query&thd=yes&row=:limit', {query: '@query', limit: '@limit'},
     {
       search: {
         method: 'GET'
