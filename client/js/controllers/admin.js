bec319ea40e84069421bd4bc9c907e20836b2b86
Michiel Hildebrand <m.hildebrand@cwi.nl>
2014-11-16 21:44:39 +0100

    Fix line endings

--- client/js/controllers/admin.js
+++ client/js/controllers/admin.js
@@ -1,1 +1,15 @@
-'use strict';angular.module('AdminCtrl', []).controller('AdminCtrl', ['$scope', '$http', 'Config', '$log', adminCtrl]);function adminCtrl($scope, $http, Config, $log) {        $http.get(Config.seed).success(function(data) {        $scope.videos = data;    });  function debug(msg) {    $log.debug('[Admin (ctrl)] ' + msg)  }}
+'use strict';
+
+angular.module('AdminCtrl', []).controller('AdminCtrl', ['$scope', '$http', 'Config', '$log', adminCtrl]);
+
+function adminCtrl($scope, $http, Config, $log) {
+    
+    $http.get(Config.seed).success(function(data) {
+        $scope.videos = data;
+    });
+
+  function debug(msg) {
+    $log.debug('[Admin (ctrl)] ' + msg)
+  }
+
+}
