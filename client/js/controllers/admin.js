5e005c6b6fb67d7079c18c46afb726da66da7f1a
Michiel Hildebrand <m.hildebrand@vu.nl>
2015-01-13 17:36:10 +0100

    Controls to Add and Remove videos

--- client/js/controllers/admin.js
+++ client/js/controllers/admin.js
@@ -3,12 +3,36 @@
 angular.module('AdminCtrl', []).controller('AdminCtrl', ['$scope', '$http', 'Config', '$log', adminCtrl]);
 
 function adminCtrl($scope, $http, Config, $log) {
-    console.log("test");
+  $scope.data = {
+  	showDelete: false,
+  	showReorder: false,
+  	videos: []
+  }
 
   $http.get(Config.seed).success(function(data) {
-    console.log(data);
-    $scope.videos = data;
+    $scope.data.videos = data;
   });
+
+  $scope.removeVideo = function(index, id) {
+  	$scope.data.videos.splice(index,1)
+  	$http.get(Config.DOCUMENT_PROXY+'/rbb/remove/'+id).success(function(data) {
+  		debug('removed '+id)
+  	})
+  }
+
+	$scope.addVideo = function() {
+		var id = $scope.newVideo;
+		console.log(id);
+  	$http.get(Config.DOCUMENT_PROXY+'/rbb/add/'+id).success(function(data) {
+  		$scope.data.videos.push(data);
+  	})
+  }  
+
+  $scope.reorderVideo = function(id, from, to) {
+
+  }
+
+
 
   function debug(msg) {
     $log.debug('[Admin (ctrl)] ' + msg)
